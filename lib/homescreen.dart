60ba065f2f649dc575addde298e5c04f1819a935
ab_patil_1 <abhishekpatil1457@gmail.com>
2026-01-18 22:43:38 +0530

    update

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -73,3 +73,74 @@ class _HomescreenState extends State<Homescreen> {
     );
   }
 }
+
+
+// Future<List<String>> fetchCategories() async {
+//   final url = Uri.parse('$baseUrl/GetCategory');
+//
+//   final response = await http.get(url);
+//
+//   if (response.statusCode == 200) {
+//     final jsonBody = json.decode(response.body);
+//     final List<dynamic> data = jsonBody['Data'];
+//
+//     return data.map((item) => item['CategoryName'] as String).toList();
+//   } else {
+//     throw Exception('Failed to load categories:${response.reasonPhrase}');
+//   }
+// }
+
+
+// Future<Map<String, dynamic>> addNewContact(
+//     AddContactModel addContactModel) async {
+//   try {
+//     final requestBody = {
+//       "companyName": addContactModel.companyName,
+//       "contactName": addContactModel.contactName,
+//       "phone": addContactModel.phone,
+//       "mobile": addContactModel.mobile,
+//       "email": addContactModel.email,
+//     };
+//
+//     logGreen(msg: "addContact request body ------> $requestBody");
+//
+//     final headers = {'Content-Type': 'application/json'};
+//
+//     final url = Uri.parse('$baseUrl/');
+//
+//     showLog(msg: "$baseUrl/"); /
+//
+//     final response = await http.post(
+//       url,
+//       headers: headers,
+//       body: jsonEncode(requestBody),
+//     );
+//     if (response.statusCode == 200) {
+//       return jsonDecode(response.body);
+//     } else {
+//       logRed(msg: "add contact error ---> ${response.body}");
+//       throw Exception('Failed to addContact ${response.statusCode}');
+//     }
+//   } catch (e) {
+//     logRed(msg: "add contact error ---> $e");
+//     throw Exception('Failed to addContact $e');
+//   }
+// }
+
+// import 'dart:developer';
+//
+// void showLog({required String msg}) {
+//   // log('\x1B[35m$msg\x1B[0m');
+// }
+//
+// void logRed({required String msg}) {
+//   // log('\x1B[31m$msg\x1B[0m');
+// }
+//
+// void logGreen({required String msg}) {
+//   // log('\x1B[32m$msg\x1B[0m');
+// }
+//
+// void logBlue({required String msg}) {
+//   // log('\x1B[34m$msg\x1B[0m');
+// }
